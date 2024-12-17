667e7d0254a3f7de64f4a3e43962b19ae2ebacb4
JuanBuri <jpburi@sudamericano.edu.ec>
2024-12-16 23:33:02 -0500

    firts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: StartComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent}
+];
